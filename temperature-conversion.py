54ea806eb36d0011f6deb141fa2fcce0976809ab
leoharis <leoharis@live.co.uk>
2018-09-03 20:27:56 +0100

    Initial commit

---
+++ temperature-conversion.py
@@ -0,0 +1,15 @@
+# Temperature conversion
+def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * 5/9
+    return c_temp
+
+
+f100_in_celsius = f_to_c(100)
+
+
+def c_to_f(c_temp):
+    f_temp = c_temp * (9/5) + 32
+    return f_temp
+
+
+c0_in_fahrenheit = c_to_f(0)
