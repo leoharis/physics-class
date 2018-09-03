fad038bbf20e213b5b8d1e988190982cee1a5784
leoharis <leoharis@live.co.uk>
2018-09-03 20:43:08 +0100

    Added code for use the force

---
+++ use-the-force.py
@@ -0,0 +1,24 @@
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+
+bomb_mass = 1
+
+
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+
+train_force = get_force(train_mass, train_acceleration)
+
+print(train_force)
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+
+def get_energy(mass, c=3*10**8):
+    return mass * c ** 2
+
+
+bomb_energy = get_energy(bomb_mass)
+
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
