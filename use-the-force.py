3f1c2591cec7bbb2a0ea7619ee17c85f9b4dc661
leoharis <leoharis@live.co.uk>
2018-09-04 15:06:41 +0100

    Completed Physics Class exercise

--- use-the-force.py
+++ use-the-force.py
@@ -22,3 +22,13 @@ def get_energy(mass, c=3*10**8):
 bomb_energy = get_energy(bomb_mass)
 
 print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
+
+
+def get_work(mass, acceleration, distance):
+    force = get_force(mass, acceleration) * distance
+    return force
+
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
